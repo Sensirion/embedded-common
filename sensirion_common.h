7afafb665ff0bf526aaa893a0e13c7e73f7c7d63
Raphael Nestler <raphael.nestler@gmail.com>
2020-07-16 15:25:55 +0200

    Remove legacy endianness conversion macros

--- sensirion_common.h
+++ sensirion_common.h
@@ -43,33 +43,6 @@ extern "C" {
 #define STATUS_OK 0
 #define STATUS_FAIL (-1)
 
-#if SENSIRION_BIG_ENDIAN
-#define be16_to_cpu(s) (s)
-#define be32_to_cpu(s) (s)
-#define be64_to_cpu(s) (s)
-#define SENSIRION_WORDS_TO_BYTES(a, w) ()
-
-#else /* SENSIRION_BIG_ENDIAN */
-
-#define be16_to_cpu(s) (((uint16_t)(s) << 8) | (0xff & ((uint16_t)(s)) >> 8))
-#define be32_to_cpu(s) \
-    (((uint32_t)be16_to_cpu(s) << 16) | (0xffff & (be16_to_cpu((s) >> 16))))
-#define be64_to_cpu(s)                  \
-    (((uint64_t)be32_to_cpu(s) << 32) | \
-     (0xffffffff & ((uint64_t)be32_to_cpu((s) >> 32))))
-/**
- * Convert a word-array to a bytes-array, effectively reverting the
- * host-endianness to big-endian
- * @a:  word array to change (must be (uint16_t *) castable)
- * @w:  number of word-sized elements in the array (SENSIRION_NUM_WORDS(a)).
- */
-#define SENSIRION_WORDS_TO_BYTES(a, w)                                  \
-    for (uint16_t* __a = (uint16_t*)(a), __e = (w), __w = 0; __w < __e; \
-         ++__w) {                                                       \
-        __a[__w] = be16_to_cpu(__a[__w]);                               \
-    }
-#endif /* SENSIRION_BIG_ENDIAN */
-
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
 #endif
