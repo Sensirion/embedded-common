df716dccd53ff5e3ad786a15a66f709741c9a24b
Andreas Kreuzmann <andreas.kreuzmann@sensirion.com>
2020-10-06 12:29:07 +0200

    Add function sensirion_bytes_to_uint16_t
    Add function sensirion_bytes_to_uint16_t since it's needed for the
    SEN44 driver and in general a useful addition.

--- sensirion_common.c
+++ sensirion_common.c
@@ -38,6 +38,10 @@
 #include "sensirion_common.h"
 #include "sensirion_i2c.h"
 
+uint16_t sensirion_bytes_to_uint16_t(const uint8_t* bytes) {
+    return (uint16_t)bytes[0] << 8 | (uint16_t)bytes[1];
+}
+
 uint32_t sensirion_bytes_to_uint32_t(const uint8_t* bytes) {
     return (uint32_t)bytes[0] << 24 | (uint32_t)bytes[1] << 16 |
            (uint32_t)bytes[2] << 8 | (uint32_t)bytes[3];
