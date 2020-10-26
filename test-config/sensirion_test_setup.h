7e7408de54469c7ca1c17f25bb280f72c8c4700d
Raphael Nestler <raphael.nestler@sensirion.com>
2020-10-26 14:56:01 +0100

    Remove deprecated i2c_reset
    Just use sensirion_i2c_general_call_reset  directly.

--- test-config/sensirion_test_setup.h
+++ test-config/sensirion_test_setup.h
@@ -10,8 +10,6 @@
 #define CHECK_ZERO(actual) CHECK_EQUAL(0, (actual))
 #define CHECK_ZERO_TEXT(actual, text) CHECK_EQUAL_TEXT(0, (actual), (text))
 
-int16_t i2c_reset();
-
 int main(int argc, char** argv);
 
 #endif /* SENSIRION_TEST_SETUP_H */
