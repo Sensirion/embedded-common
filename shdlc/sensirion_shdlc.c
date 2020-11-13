e1d236cdf4de947282f8ab20fd8c172b5ed270b8
Andreas Kreuzmann <andreas.kreuzmann@sensirion.com>
2020-11-13 11:11:29 +0100

    Rename functions in sensirion_uart_hal

--- shdlc/sensirion_shdlc.c
+++ shdlc/sensirion_shdlc.c
@@ -111,7 +111,7 @@ int16_t sensirion_shdlc_xcv(uint8_t addr, uint8_t cmd, uint8_t tx_data_len,
     if (ret != 0)
         return ret;
 
-    sensirion_sleep_usec(RX_DELAY_US);
+    sensirion_uart_hal_sleep_usec(RX_DELAY_US);
     return sensirion_shdlc_rx(max_rx_data_len, rx_header, rx_data);
 }
 
@@ -132,7 +132,7 @@ int16_t sensirion_shdlc_tx(uint8_t addr, uint8_t cmd, uint8_t data_len,
     len += sensirion_shdlc_stuff_data(1, &crc, tx_frame_buf + len);
     tx_frame_buf[len++] = SHDLC_STOP;
 
-    ret = sensirion_uart_tx(len, tx_frame_buf);
+    ret = sensirion_uart_hal_tx(len, tx_frame_buf);
     if (ret < 0)
         return ret;
     if (ret != len)
@@ -151,7 +151,7 @@ int16_t sensirion_shdlc_rx(uint8_t max_data_len,
     uint8_t crc;
     uint8_t unstuff_next;
 
-    len = sensirion_uart_rx(2 + (5 + (uint16_t)max_data_len) * 2, rx_frame);
+    len = sensirion_uart_hal_rx(2 + (5 + (uint16_t)max_data_len) * 2, rx_frame);
     if (len < 1 || rx_frame[0] != SHDLC_START)
         return SENSIRION_SHDLC_ERR_MISSING_START;
 
