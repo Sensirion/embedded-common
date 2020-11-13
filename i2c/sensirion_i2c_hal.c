0ee4fa845390ec41a2fec2c4379c88907fccb84a
Andreas Kreuzmann <andreas.kreuzmann@sensirion.com>
2020-11-13 11:33:42 +0100

    Rename functions in sensirion_i2c_hal

--- i2c/sensirion_i2c_hal.c
+++ i2c/sensirion_i2c_hal.c
@@ -51,7 +51,7 @@
  * @param bus_idx   Bus index to select
  * @returns         0 on success, an error code otherwise
  */
-int16_t sensirion_i2c_select_bus(uint8_t bus_idx) {
+int16_t sensirion_i2c_hal_select_bus(uint8_t bus_idx) {
     // TODO:IMPLEMENT or leave empty if all sensors are located on one single
     // bus
     return NOT_IMPLEMENTED_ERROR;
@@ -61,14 +61,14 @@ int16_t sensirion_i2c_select_bus(uint8_t bus_idx) {
  * Initialize all hard- and software components that are needed for the I2C
  * communication.
  */
-void sensirion_i2c_init(void) {
+void sensirion_i2c_hal_init(void) {
     // TODO:IMPLEMENT
 }
 
 /**
- * Release all resources initialized by sensirion_i2c_init().
+ * Release all resources initialized by sensirion_i2c_hal_init().
  */
-void sensirion_i2c_release(void) {
+void sensirion_i2c_hal_free(void) {
     // TODO:IMPLEMENT or leave empty if no resources need to be freed
 }
 
@@ -82,7 +82,7 @@ void sensirion_i2c_release(void) {
  * @param count   number of bytes to read from I2C and store in the buffer
  * @returns 0 on success, error code otherwise
  */
-int8_t sensirion_i2c_read(uint8_t address, uint8_t* data, uint16_t count) {
+int8_t sensirion_i2c_hal_read(uint8_t address, uint8_t* data, uint16_t count) {
     // TODO:IMPLEMENT
     return NOT_IMPLEMENTED_ERROR;
 }
@@ -98,8 +98,8 @@ int8_t sensirion_i2c_read(uint8_t address, uint8_t* data, uint16_t count) {
  * @param count   number of bytes to read from the buffer and send over I2C
  * @returns 0 on success, error code otherwise
  */
-int8_t sensirion_i2c_write(uint8_t address, const uint8_t* data,
-                           uint16_t count) {
+int8_t sensirion_i2c_hal_write(uint8_t address, const uint8_t* data,
+                               uint16_t count) {
     // TODO:IMPLEMENT
     return NOT_IMPLEMENTED_ERROR;
 }
@@ -112,6 +112,6 @@ int8_t sensirion_i2c_write(uint8_t address, const uint8_t* data,
  *
  * @param useconds the sleep time in microseconds
  */
-void sensirion_sleep_usec(uint32_t useconds) {
+void sensirion_i2c_hal_sleep_usec(uint32_t useconds) {
     // TODO:IMPLEMENT
 }
