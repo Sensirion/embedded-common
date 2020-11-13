8cc5fe55099e96ae8247d5105ecacbe9c22f3807
Andreas Kreuzmann <andreas.kreuzmann@sensirion.com>
2020-11-13 11:00:48 +0100

    Rename functions in sensirion_common

--- common/sensirion_common.c
+++ common/sensirion_common.c
@@ -32,64 +32,64 @@
 #include "sensirion_common.h"
 #include "sensirion_config.h"
 
-uint16_t sensirion_bytes_to_uint16_t(const uint8_t* bytes) {
+uint16_t sensirion_common_bytes_to_uint16_t(const uint8_t* bytes) {
     return (uint16_t)bytes[0] << 8 | (uint16_t)bytes[1];
 }
 
-uint32_t sensirion_bytes_to_uint32_t(const uint8_t* bytes) {
+uint32_t sensirion_common_bytes_to_uint32_t(const uint8_t* bytes) {
     return (uint32_t)bytes[0] << 24 | (uint32_t)bytes[1] << 16 |
            (uint32_t)bytes[2] << 8 | (uint32_t)bytes[3];
 }
 
-int16_t sensirion_bytes_to_int16_t(const uint8_t* bytes) {
-    return (int16_t)sensirion_bytes_to_uint16_t(bytes);
+int16_t sensirion_common_bytes_to_int16_t(const uint8_t* bytes) {
+    return (int16_t)sensirion_common_bytes_to_uint16_t(bytes);
 }
 
-int32_t sensirion_bytes_to_int32_t(const uint8_t* bytes) {
-    return (int32_t)sensirion_bytes_to_uint32_t(bytes);
+int32_t sensirion_common_bytes_to_int32_t(const uint8_t* bytes) {
+    return (int32_t)sensirion_common_bytes_to_uint32_t(bytes);
 }
 
-float sensirion_bytes_to_float(const uint8_t* bytes) {
+float sensirion_common_bytes_to_float(const uint8_t* bytes) {
     union {
         uint32_t u32_value;
         float float32;
     } tmp;
 
-    tmp.u32_value = sensirion_bytes_to_uint32_t(bytes);
+    tmp.u32_value = sensirion_common_bytes_to_uint32_t(bytes);
     return tmp.float32;
 }
 
-void sensirion_uint32_t_to_bytes(const uint32_t value, uint8_t* bytes) {
+void sensirion_common_uint32_t_to_bytes(const uint32_t value, uint8_t* bytes) {
     bytes[0] = value >> 24;
     bytes[1] = value >> 16;
     bytes[2] = value >> 8;
     bytes[3] = value;
 }
 
-void sensirion_uint16_t_to_bytes(const uint16_t value, uint8_t* bytes) {
+void sensirion_common_uint16_t_to_bytes(const uint16_t value, uint8_t* bytes) {
     bytes[0] = value >> 8;
     bytes[1] = value;
 }
 
-void sensirion_int32_t_to_bytes(const int32_t value, uint8_t* bytes) {
+void sensirion_common_int32_t_to_bytes(const int32_t value, uint8_t* bytes) {
     bytes[0] = value >> 24;
     bytes[1] = value >> 16;
     bytes[2] = value >> 8;
     bytes[3] = value;
 }
 
-void sensirion_int16_t_to_bytes(const int16_t value, uint8_t* bytes) {
+void sensirion_common_int16_t_to_bytes(const int16_t value, uint8_t* bytes) {
     bytes[0] = value >> 8;
     bytes[1] = value;
 }
 
-void sensirion_float_to_bytes(const float value, uint8_t* bytes) {
+void sensirion_common_float_to_bytes(const float value, uint8_t* bytes) {
     union {
         uint32_t u32_value;
         float float32;
     } tmp;
     tmp.float32 = value;
-    sensirion_uint32_t_to_bytes(tmp.u32_value, bytes);
+    sensirion_common_uint32_t_to_bytes(tmp.u32_value, bytes);
 }
 
 void sensirion_common_copy_bytes(const uint8_t* source, uint8_t* destination,
